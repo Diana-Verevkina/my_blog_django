46e5bfc7a0c3c74109f80a667d56298d41f58bbd
Diana-Verevkina <diva2208@mail.ru>
2022-07-17 21:56:05 +0300

    create pages

--- blog/blog_app/views.py
+++ blog/blog_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    template = 'posts/index.html'
+    return render(request, template)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    return render(request, template)
